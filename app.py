7c923237716a895587fca9a6a2c3157d9cbb4bff
Sebastião Oliveira <tiaorj@gmail.com>
2026-04-24 15:36:48 -0300

    Meu primeiro site com Flask e Bootstrap

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    dados_curriculo = {
+        'nome': 'Sebastião G. Oliveira',
+        'cargo': 'Desenvolvedor Full Stack',
+        'habilidades': ['Python', 'Flask', 'PostgreSQL', 'HTML5']
+    }
+    return render_template('index.html', info=dados_curriculo)
